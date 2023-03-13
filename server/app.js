89e9bb31d25d18d97665f9edfe1cefe79526ef69
Ignacio Diaz <diaz.ignacio023@gmail.com>
2023-03-13 13:25:59 -0300

    chore: first commit

---
+++ server/app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB database
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log(err));
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+// app.use('/api/users', require('./routes/users'));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
+
+// Start the server
+app.listen(port, () => console.log(`Server listening on port ${port}`));
